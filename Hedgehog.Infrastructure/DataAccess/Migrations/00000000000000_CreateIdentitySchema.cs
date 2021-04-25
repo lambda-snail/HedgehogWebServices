d3f752299f04bef1a9a604ea690338d2f6e93e4e
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-04-25 15:15:18 +0200

    DbCOntext move to Infrastructure - part 2

--- Hedgehog.Infrastructure/DataAccess/Migrations/00000000000000_CreateIdentitySchema.cs
+++ Hedgehog.Infrastructure/DataAccess/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace Hedgehog.UI.Data.Migrations
+namespace Hedgehog.Infrastructure.DataAccess.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
