8d20ea3a07d21b78d694a4c6601fa49eb3e06779
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-05-03 00:12:12 +0200

    Added a details page for products.

--- Hedgehog.UI/Controllers/StoreController.cs
+++ Hedgehog.UI/Controllers/StoreController.cs
@@ -5,6 +5,7 @@ using Hedgehog.Core.Domain;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
+using Hedgehog.UI.ViewModels;
 
 namespace Hedgehog.UI.Controllers
 {
@@ -20,6 +21,7 @@ namespace Hedgehog.UI.Controllers
             _mediator = mediator;
         }
 
+        //[Route("{storeNavigationTitle}/Store/Index")]
         public async Task<IActionResult> Index(string storeNavigationTitle)
         {
             WebStore store =  await _mediator.Send( new GetStoreFromNavigationTitleRequest { NavigationTitle = storeNavigationTitle } );
@@ -33,6 +35,35 @@ namespace Hedgehog.UI.Controllers
             ViewBag.Message = store.StoreTitle;
             ViewBag.NumItems = products.Count();
             return View(products);
+        }
+
+        [Route("{storeNavigationTitle}/Store/Details")]
+        public async Task<IActionResult> Details(string storeNavigationTitle, int productId)
+        {
+            WebStore store = await _mediator.Send(new GetStoreFromNavigationTitleRequest { NavigationTitle = storeNavigationTitle });
+
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            Product p = await _mediator.Send( new GetSingleProductFromStoreRequest { StoreId=store.WebStoreId, ProductId=productId } );
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            ProductViewModel productVm = new();
+            productVm.ProductName = p.ProductName;
+            productVm.ShortDescription = p.ShortDescription;
+            productVm.LongDescription = p.LongDescription;
+            productVm.ImageUrl = p.ImageUrl;
+            productVm.Price = p.Price;
+
+            ViewBag.ProductId = p.ProductId;
+
+            return View(productVm);
         }
     }
 }
