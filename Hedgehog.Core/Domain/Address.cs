36111bf05b9c047137fee7d88abee5dd22c3ba15
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-05-03 17:03:25 +0200

    Added classes to represetn orders, items in an order and address.

---
+++ Hedgehog.Core/Domain/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace Hedgehog.Core.Domain
+{
+    /// <summary>
+    /// A stub implementation for a generic address for demo purposes. It is designed to look like an average address 
+    /// that you would find in most countries, but without the details.
+    /// </summary>
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Receiver { get; set; }
+        public string StreetAddress { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Hedgehog.Core/Domain/Order.cs
@@ -0,0 +1,27 @@
+﻿using Hedgehog.Core.Application;
+using System;
+using System.Collections.Generic;
+
+namespace Hedgehog.Core.Domain
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime? FinalizedTime { get; set; } = null;
+
+        public List<OrderItem> OrderItems { get; set; }
+        public Address ShippingAddress { get; set; }
+
+        public CustomerAccount Customer { get; set; }
+
+        /// <summary>
+        /// Returns true if and only if the transaction associated with this order is completed.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsCompletedTransaction()
+        {
+            return FinalizedTime == null;
+        }
+    }
+}
