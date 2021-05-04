406da512ac34878b07b97b43f961d5f78fc77f2d
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-05-04 12:10:18 +0200

    Some minor tweaks.

---
+++ Hedgehog.UI/ViewModels/PaymentSummaryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hedgehog.UI.ViewModels
+{
+    /// <summary>
+    /// Contains all the data needed to properly render the payment screen to the user.
+    /// </summary>
+    public class PaymentSummaryViewModel
+    {
+        public string WebStoreName { get; set; }
+        public double TotalAmount { get; set; }
+    }
+}
