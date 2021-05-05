ff737ea1fb53189e88969f93c71824035a7e5807
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-05-05 15:53:10 +0200

    Added a repository for Order items.

---
+++ Hedgehog.Infrastructure/DataAccess/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Hedgehog.Core.Contracts.InfrastructureContracts.Repositories;
+using Hedgehog.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hedgehog.Infrastructure.DataAccess
+{
+    public class OrderRepository : RepositoryBase<Order, int>, IOrderRepository
+    {
+        public OrderRepository(ApplicationDbContext database) : base(database) {}
+
+        public async Task<IEnumerable<Order>> GetOrderByCustomerIdAsync(string userId)
+        {
+            return await _database.Set<Order>().Include(order => order.Customer)
+                                  .Include(order => order.ShippingAddress)
+                                  .Include(order => order.OrderItems)
+                                  .Where(order => order.Customer.Id == userId)
+                                  .ToListAsync();
+        }
+    }
+}
