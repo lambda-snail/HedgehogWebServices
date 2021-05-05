413d0d2e8214a166d94ae43e90c242451fd51f38
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-05-05 15:54:57 +0200

    Added some more unit tests and mock data.

---
+++ HedgehogTesting/UnitTests/CoreUnitTests/Domain/OrderRequestHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using Hedgehog.Core.Application;
+using Hedgehog.Core.Domain;
+using Hedgehog.Core.Domain.Commands;
+using Hedgehog.Core.Domain.Requests;
+using HedgehogTesting.UnitTests.TestData;
+using MediatR;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace HedgehogTesting.UnitTests.CoreUnitTests.Domain
+{
+    public class OrderRequestHandlerTests
+    {
+        [Fact]
+        public async void CreateOrderRequestHandlerTest_ValidData_NoSaveToDb()
+        {
+            Address address = AddressTestData.Get();
+            WebStore store = WebStoreTestData.Get();
+            CustomerAccount customer = CustomerTestData.Get("00000000-0000-0000-0000-000000000000", store);
+            ShoppingCart cart = ShoppingCartTestData.Get(store.WebStoreId, new List<int> { 1, 2, 3, 4, 5 });
+
+            var request = new CreateOrderRequest { Address=address, Cart=cart, Customer=customer, SaveToDatabase=false };
+
+            // Since the handler uses another call to mediator to get a product from the database, we need to
+            // mock that behavior here
+            var mediator = new Mock<IMediator>();
+            mediator.Setup(m => m.Send(It.IsAny<GetSingleProductFromStoreRequest>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync((GetSingleProductFromStoreRequest request, CancellationToken token) =>
+                        new Product { ProductId = request.ProductId, ProductName = "test", ShortDescription = "foobar", Price = 10.0d, WebStoreId = store.WebStoreId, WebStore = store }
+                    );
+
+            var handler = new CreateOrderRequestHandler( mediator.Object );
+
+            Order result = await handler.Handle(request, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Equal(cart.Items.Count(), result.OrderItems.Count());
+        }
+    }
+}
---
+++ HedgehogTesting/UnitTests/TestData/AddressTestData.cs
@@ -0,0 +1,12 @@
+﻿using Hedgehog.Core.Domain;
+
+namespace HedgehogTesting.UnitTests.TestData
+{
+    class AddressTestData
+    {
+        public static Address Get()
+        {
+            return new Address { AddressId = 1, City = "Stockholm", Country = "Sweden", Receiver = "Smith", StreetAddress = "Storgatan", ZipCode = "11111" };
+        }
+    }
+}
