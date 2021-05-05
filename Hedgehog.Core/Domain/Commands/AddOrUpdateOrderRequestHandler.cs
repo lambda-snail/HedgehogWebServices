0d3db88d397e9ab01f3858ba77addb3135204fea
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-05-05 15:54:04 +0200

    Added commands to create and update orders via MediatR.

---
+++ Hedgehog.Core/Domain/Commands/AddOrUpdateOrderRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using Hedgehog.Core.Contracts.InfrastructureContracts.Repositories;
+using Hedgehog.Core.Domain.Requests;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hedgehog.Core.Domain.Commands
+{
+    /// <summary>
+    /// Adds a new Order to the system or updates an existing. The new or updated order is returned after execution.
+    /// </summary>
+    public class AddOrUpdateOrderRequestHandler : IRequestHandler<AddOrUpdateOrderRequest>
+    {
+        private readonly IMediator _mediator;
+        private readonly IAsyncGenericRepository<Order, int> _orderRepository;
+
+        public AddOrUpdateOrderRequestHandler(IMediator mediator, IAsyncGenericRepository<Order, int> orderRepository)
+        {
+            _mediator = mediator;
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Unit> Handle(AddOrUpdateOrderRequest request, CancellationToken cancellationToken)
+        {
+            await _orderRepository.UpdateAsync(request.Order);
+            return Unit.Value;
+        }
+    }
+}
---
+++ Hedgehog.Core/Domain/Requests/CreateOrderRequest.cs
@@ -0,0 +1,18 @@
+﻿using Hedgehog.Core.Application;
+using MediatR;
+
+namespace Hedgehog.Core.Domain.Requests
+{
+    public class CreateOrderRequest : IRequest<Order>
+    {
+        public ShoppingCart Cart { get; set; }
+        public Address Address { get; set; }
+        public CustomerAccount Customer { get; set; }
+
+        /// <summary>
+        /// Should the resulting order be saved to the database when created?
+        /// Defaults to false.
+        /// </summary>
+        public bool SaveToDatabase { get; set; } = false;
+    }
+}
