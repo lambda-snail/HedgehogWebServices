97dd8b4a7864083ba557179b1a9e1c708fb72917
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-05-09 18:32:06 +0200

    Formatting fixes.

--- Hedgehog.Infrastructure/DataAccess/OrderRepository.cs
+++ Hedgehog.Infrastructure/DataAccess/OrderRepository.cs
@@ -11,13 +11,27 @@ namespace Hedgehog.Infrastructure.DataAccess
     {
         public OrderRepository(ApplicationDbContext database) : base(database) {}
 
-        public async Task<IEnumerable<Order>> GetOrderByCustomerIdAsync(string userId)
+        public async Task<IEnumerable<Order>> GetOrdersByCustomerIdAsync(string customerId)
         {
-            return await _database.Set<Order>().Include(order => order.Customer)
+            return await _database.Set<Order>().Where(order => order.Customer.Id == customerId)
+                                  .Include(order => order.Customer)
                                   .Include(order => order.ShippingAddress)
                                   .Include(order => order.OrderItems)
-                                  .Where(order => order.Customer.Id == userId)
                                   .ToListAsync();
+        }
+
+        /// <summary>
+        /// Override base class method to allow inclusion of all navigation properties.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public override async Task<Order> GetByIdAsync(int id)
+        {
+            return await _database.Set<Order>()
+                                  .Include(order => order.Customer)
+                                  .Include(order => order.ShippingAddress)
+                                  .Include(order => order.OrderItems)
+                                  .FirstOrDefaultAsync(order => order.OrderId == id);
         }
     }
 }
