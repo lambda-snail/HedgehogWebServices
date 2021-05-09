5750d5bfd5a88bbbb2f51754bbebb772adcc723f
lambda-snail <79707893+lambda-snail@users.noreply.github.com>
2021-05-09 15:18:24 +0200

    Added request to retreive order from DB by Id.

---
+++ Hedgehog.Core/Domain/Commands/GetOrderByIdRequestHandler.cs
@@ -0,0 +1,23 @@
+﻿using Hedgehog.Core.Contracts.InfrastructureContracts.Repositories;
+using Hedgehog.Core.Domain.Requests;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hedgehog.Core.Domain.Commands
+{
+    public class GetOrderByIdRequestHandler : IRequestHandler<GetOrderByIdRequest, Order>
+    {
+        private readonly IAsyncGenericRepository<Order, int> _orderRepository;
+
+        public GetOrderByIdRequestHandler(IAsyncGenericRepository<Order, int> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public Task<Order> Handle(GetOrderByIdRequest request, CancellationToken cancellationToken)
+        {
+            return _orderRepository.GetByIdAsync(request.OrderId);
+        }
+    }
+}
